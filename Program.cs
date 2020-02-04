1e146cfdc910075db4f9a889976c7f73edc8ae55
Mark Ellis <markellis@quickenloans.com>
2020-02-04 10:23:41 -0500

    Lab3.3 Data Structures

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab3._3DataStructures2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Hello, please enter a few words: ");
+            string text = Console.ReadLine();
+
+            Console.WriteLine(ReverseString(text));
+
+
+        }
+
+        public static string ReverseString(string text)
+        {
+
+            //Created a stack of Char
+            Stack<char> stack = new Stack<char>();
+
+
+            //Used foreach loop to push each character in text string to stack.
+            foreach (char c in text)
+            {
+                stack.Push(c);
+            }
+
+            //used string.Empty to clear value in text.
+            text = string.Empty;
+
+            //Used a while loop to pop each character in the stack back to text string.
+            while (stack.Count > 0)
+            {
+                text += stack.Pop();
+            }
+
+            return text;
+
+
+        }
+    }
+}
